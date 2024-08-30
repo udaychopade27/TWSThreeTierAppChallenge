decd7fd6203e0d753ac1bb289fc83031e6a4def5
udaychopade27 <udaychopade27@gmail.com>
2024-08-30 12:13:22 +0530

    added env file and configure it

--- backend/db.js
+++ backend/db.js
@@ -3,23 +3,20 @@ const mongoose = require("mongoose");
 module.exports = async () => {
     try {
         const connectionParams = {
-            // user: process.env.MONGO_USERNAME,
-            // pass: process.env.MONGO_PASSWORD,
             useNewUrlParser: true,
-            // useCreateIndex: true,
             useUnifiedTopology: true,
         };
+
         const useDBAuth = process.env.USE_DB_AUTH || false;
-        if(useDBAuth){
+        if (useDBAuth) {
             connectionParams.user = process.env.MONGO_USERNAME;
             connectionParams.pass = process.env.MONGO_PASSWORD;
         }
-        await mongoose.connect(
-           process.env.MONGO_CONN_STR,
-           connectionParams
-        );
+
+        await mongoose.connect(process.env.MONGO_URI, connectionParams);
         console.log("Connected to database.");
     } catch (error) {
         console.log("Could not connect to database.", error);
     }
 };
+
